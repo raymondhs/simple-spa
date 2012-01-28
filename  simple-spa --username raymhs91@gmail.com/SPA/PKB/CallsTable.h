770db8770ed354c310a49cfccb4e33af2b9c2a5a
peter@zeroexistence.org <peter@zeroexistence.org@659f77e0-993d-4cc1-9a52-bb51f35ee826>
2012-01-28 08:42:46 +0000

    git-svn-id: file:///Users/raymondhs/Workspace/p/trunk@41 659f77e0-993d-4cc1-9a52-bb51f35ee826

---  simple-spa --username raymhs91@gmail.com/SPA/PKB/CallsTable.h
+++  simple-spa --username raymhs91@gmail.com/SPA/PKB/CallsTable.h
@@ -12,8 +12,29 @@ private:
 	CallsTable(const CallsTable &);
 	CallsTable& operator=(const CallsTable &);
 public:
+
+	/**
+	 * Gets a reference to calls table
+	 *
+	 * @return Reference to calls table
+	 */
 	static CallsTable* getCallsTable();
+
+	/**
+	 * Insert the index of the calls statement which calls a procedure
+	 *
+	 * @param stmt Index of the caller statement
+	 * @param proc Index of the procedure called
+	 */
 	void insertStmt(STMT_NO stmt, PROC_IDX proc);
+
+	/**
+	 * Insert the index of the procedure which calls another procedure
+	 *
+	 * @param proc1 Index of the caller procedure
+	 * @param proc2 Index of the procedure called
+	 */
 	void insertProc(PROC_IDX proc1, PROC_IDX proc2);
+
 	void clearTable();
 };
