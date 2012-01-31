b7a2a25ea4470177207e63b0166d470f6f9ae4f5
richard.bluez <richard.bluez@659f77e0-993d-4cc1-9a52-bb51f35ee826>
2012-01-31 08:47:31 +0000

    git-svn-id: file:///Users/raymondhs/Workspace/p/trunk@80 659f77e0-993d-4cc1-9a52-bb51f35ee826

---  simple-spa --username raymhs91@gmail.com/SPA/PKB/GNode.h
+++  simple-spa --username raymhs91@gmail.com/SPA/PKB/GNode.h
@@ -6,7 +6,8 @@ class GNode
 {
 private:
 	int attribute;
-	std::set<int> next;
+	std::set<GNode*> next;
+	std::set<GNode*> previous;
 	void init();
 public:
 	GNode(void);
@@ -14,10 +15,10 @@ public:
 	GNode createNode(int attribute);
 	void setNext(GNode *node);
 	void setPrevious(GNode *node);
-	std::set<int> getNext();
-	std::set<int> getNextTransitive();
-	std::set<int> getPrevious();
-	std::set<int> getPreviousTransitive();
+	std::set<GNode*> getNext();
+	std::set<GNode*> getNextTransitive();
+	std::set<GNode*> getPrevious();
+	std::set<GNode*> getPreviousTransitive();
 	bool isNext(GNode node1, GNode node2);
 	bool isNextTransitive(GNode node1, GNode node2);
 };
